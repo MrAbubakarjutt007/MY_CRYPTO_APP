e9c5a4fab0c27ed4db78c14ed4f384cd19895e25
Mrabubakarjutt007 <abubakarjuttabubakar553@gmail.com>
2023-08-17 02:57:32 -0700

    some changes

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,44 +1,14 @@
 import React from "react";
 import "./coin/Coindata.css"
-import Slider from "react-slick";
-
-function Coin({ name, icon, price, symbol }) {
-  const settings = {
-    infinite:true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          
-        }
-      }
-      
-    ]
-  };
-
-
+function Coin(props) {
   return (
       
     <div className="container">
     <div className="coin">
-        <img src={icon} />
-        <h1> Name:{name}</h1>
-        <h3> Price:{price}</h3>
-        <h3> Symbol:{symbol}</h3>   
+        <img src={props.icon}/>
+        <h1> Name:{props.name}</h1>
+        <h3> Price:{props.price}</h3>
+        <h3> Symbol:{props.symbol}</h3>   
     </div>
     </div>
   );
